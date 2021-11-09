36e1f13a258b6f7883520c61d61d0bd1392bd565
FyyyyK <angguretakim@gmail.com>
2021-11-09 22:40:54 +0000

    X00TD: Regenerate

--- superior_X00TD.mk
+++ superior_X00TD.mk
@@ -9,11 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Superior stuff
-$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+# Inherit some common superior stuff
+$(call inherit-product, vendor/superior/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 SUPERIOR_BUILD_TYPE := UNOFFICIAL
-TARGET_GAPPS_ARCH := arm64
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
